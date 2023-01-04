8a88e49dfe5582e8ed967ac5e7749c071c6b54e9
fatmadurak <fatmabadillidurak@gmail.com>
2023-01-04 15:04:02 +0300

    commited

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 
 import './App.css';
+import UserDetails from './components/UserDetails';
+import Users from './components/Users';
 
 function App() {
   return (
     <div className="App">
-  
+      
+    <div className="left">
+      <Users/>
+    </div>
+    <div className="right">
+      <UserDetails/>
+    </div>
     </div>
   );
 }
---
+++ src/components/Users.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+
+function Users() {
+
+const [users,setUsers]=useState([]);
+const [loading,setLoading]=useState(true);
+
+useEffect(()=>{
+
+
+    axios("https://jsonplaceholder.typicode.com/users")
+    .then((res)=>setUsers(res.data))
+    .finally(()=>setLoading(false))
+   
+
+},[])
+
+  return (
+
+
+    <div>
+        <h1>Kullanıcılar</h1>
+        {loading && <p>Yükleniyor...</p>}
+       <ul>
+       {
+
+        users.map((user=>{
+
+          <li key={user.id}>{user.name}</li>
+
+        }))
+        }
+       </ul>
+    </div>
+  )
+}
+
+export default Users
